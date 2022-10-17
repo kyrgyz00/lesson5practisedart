0e324bdafcee2d2717fbc28eb97dbd770132695c
kyrgyz00 <bekbolottashpolotov@gmail.com>
2022-10-17 11:14:55 +0600

    first commit

---
+++ lesson5.dart
@@ -0,0 +1,29 @@
+void main() {
+  // List c = [1, 4, 5, 30, 40, 88, 99, 123, 345];
+  // for (int b = 0; b < c.length; b++) {
+  //   //++  +=
+
+  //   //print(b);
+  //   print('index of $b');
+  //   print(c[b]);
+  //   print(c[b]*2);
+  List<String> names = [
+    'Bke',
+    'Ayba',
+    'Bra',
+    'kaka',
+    'Bka',
+    'ilya',
+  ];
+  List nameFirst = [];
+
+  for (int i = 0; i < names.length; i++) {
+    String name = names[i];
+    print(name);
+    
+    // if (names[0].contains('a') || names[0].contains('A')) {
+    //   nameFirst.add(name);
+    //   print(nameFirst);
+    // }
+  }
+}
---
+++ practise.dart
@@ -0,0 +1,141 @@
+void main() {
+  // int a = 5;
+  // double b = 5.126;
+  // double c = a - b;
+  // double k = a + b;
+  // double p = a * b;
+  // double u = a / b;
+  // print('$a-$b=$c');
+  // print('$a+$b=$k');
+  // print('$a*$b=$p');
+  // print('$a/$b=$u');
+
+  // int e = 5;
+  // int g = 2;
+  // int i = e + g;
+  // int o = e - g;
+  // print('$e+$g=$i');
+  // print('$e-$g=$i');
+
+  // int tyu = 132;
+  // int ui = 123;
+  // int ty = tyu * ui;
+  // print('$tyu*$ui=$ty');
+
+  // int uy = 78;
+  // int yt = 2;
+  // double oi = uy / yt;
+  // print('$uy/$yt=$oi');
+
+  // int re = 78;
+  // double er = 2.5;
+  // double rt = re / er;
+  // print('$re/$er=$rt');
+
+  // int we = 0;
+  // int ew = 5;
+  // int t = we * ew;
+  // print('$we*$ew=$t');
+
+  // int d = 6;
+  // int v = 90;
+  // d = d - v;
+  // v = d + v;
+  // d = v - d;
+  // print(d);
+  // print(v);
+
+  // String tor = "Bekbolot";
+  // int age = 20;
+  // print(tor);
+  // print(age);
+
+  // int nomer = 0997005111;
+  // print(nomer);
+
+  // String a = 'Rainy';
+  // if (a == 'Rainy') {
+  //   print("Take an umbrella");
+  // } else {
+  //   print("don't taje an umbrella");
+  // }
+
+  // String b = 'Rainy';
+  // print(b=='It is not rainy'?"Take an umbrella":"Don't take an umbrella");
+
+  // int a = 2;
+  // if (a > 3) {
+  //   print(a * 10);
+  // } else {
+  //   print(a / 10);
+  // }
+
+  // int a = 11;
+  // if (a < 7) {
+  //   print('Yes');
+  // }
+  //   else if  (a > 10) {
+  //   print('No');
+  // }
+  // if (a==9){
+  //   print('Error');
+  // }
+
+  // int a = 55;
+  // int b = 88;
+  // if (a > b) {
+  //   print(a);
+  // } else {
+  //   print(b);
+  // }
+
+  // int a = 55;
+  // int v = 45;
+  // if (a + v == 100) {
+  //   print('yes');
+  // } else {
+  //   print('No');
+  // }
+
+  List c = [1, 4, 5, 30, 40, 88, 99, 123, 345];
+  for (int b = 0; b < c.length; b++) {
+    // print(b);
+    //print('index of $b');
+    print(c[b]);
+    //print(c[b] * 2);
+  }
+
+  // int monthes = 2;
+  // for (int a = 0; a < 2; a++) {
+  //   print('winter');
+  // }
+  // for (int a = 2; a < 5; a++) {
+  //   print('spring');
+  // }
+
+  // int a = 65;
+  // int b = 45;
+  // int c = 34;
+
+  // int v = 654;
+  // print(v);
+
+  List<int> mon = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  for (int a = 0; a < mon.length; a++) {
+    print(mon[a]);
+  }
+  // List s = ["*", "**", "***", "****", "*****"];
+  // for (int n = 0; n < s.length; n++) {
+  //   print(s[n]);
+  // }
+
+  // List<int> d = [1, 2, 3, 4, 5];
+  // for (int s = 0; s < d.length; s++) {
+  //   print(d[s]);
+  // }
+
+  // List<int> k = [5, 4, 3, 2, 1];
+  // for (int t = 0; t < k.length; t++) {
+  //   print(k[t]);
+  // }
+}
